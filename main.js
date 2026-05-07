db677403d271107a900cb87f3dd9d43d56354962
morgan1000 <morgan.gardes.19@gmail.com>
2026-05-07 11:03:31 +0200

    ex. fonctionnel

--- main.js
+++ main.js
@@ -1,7 +1,25 @@
-let controlDiv = document.querySelector(".controls");
+let controlSpacing = document.querySelector("#spacingRange");
 
-controlDiv.addEventListener("input", handleClickOnInput);
+controlSpacing.addEventListener("input", handleClickOnInput);
 
 function handleClickOnInput() {
-    
+  let valeur2 = controlSpacing.value + controlSpacing.dataset.sizing;
+  document.documentElement.style.setProperty("--spacing", valeur2);
+}
+
+let controlBlur = document.querySelector("#blurRange");
+
+controlBlur.addEventListener("input", handleClickOnInput2);
+
+function handleClickOnInput2() {
+  let valeur = controlBlur.value + controlBlur.dataset.sizing;
+  document.documentElement.style.setProperty("--blur", valeur);
+}
+
+let controlColor = document.querySelector(".shift2");
+
+controlColor.addEventListener("input", handleClickOnInput3);
+
+function handleClickOnInput3() {
+      document.documentElement.style.setProperty("--color", controlColor.value);
 }
